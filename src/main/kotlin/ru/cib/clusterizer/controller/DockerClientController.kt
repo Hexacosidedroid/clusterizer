c1ad6ae529ac5d1f804a1acc8b974c911c5cefa8
slava_ivanov <tristandtm@gmail.com>
2025-01-28 14:23:06 +0300

    Divided controller on client, container, image controllers

---
+++ src/main/kotlin/ru/cib/clusterizer/controller/DockerClientController.kt
@@ -0,0 +1,64 @@
+package ru.cib.clusterizer.controller
+
+import com.github.dockerjava.api.model.*
+import kotlinx.coroutines.flow.Flow
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
+import ru.cib.clusterizer.domain.config.ConfigId
+import ru.cib.clusterizer.domain.docker.DockerApiService
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/docker")
+class DockerClientController(
+    private val apiServices: Map<ConfigId, DockerApiService>
+) {
+
+    @GetMapping("/client/{configId}/ping")
+    fun getPing(@PathVariable("configId") configId: ConfigId): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.ping()
+        return if (result) {
+            ResponseEntity(HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @GetMapping("/client/{configId}/info")
+    fun getInfo(@PathVariable("configId") configId: ConfigId): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.info()
+        return if (result != null) {
+            ResponseEntity(result, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @GetMapping("/client/{configId}/version")
+    fun getVersion(@PathVariable("configId") configId: ConfigId): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.version()
+        return if (result != null) {
+            ResponseEntity(result, HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @GetMapping("/client/{configId}/events", produces = [MediaType.APPLICATION_NDJSON_VALUE])
+    suspend fun events(@PathVariable("configId") configId: ConfigId): Flow<Event> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.events()
+        return result
+    }
+
+}
---
+++ src/main/kotlin/ru/cib/clusterizer/controller/DockerImageController.kt
@@ -0,0 +1,139 @@
+package ru.cib.clusterizer.controller
+
+import com.github.dockerjava.api.model.PullResponseItem
+import com.github.dockerjava.api.model.PushResponseItem
+import kotlinx.coroutines.flow.Flow
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+import ru.cib.clusterizer.dao.rest.ImageRequest
+import ru.cib.clusterizer.domain.config.ConfigId
+import ru.cib.clusterizer.domain.docker.DockerApiService
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/docker")
+class DockerImageController(
+    private val apiServices: Map<ConfigId, DockerApiService>
+) {
+
+    @PostMapping("/image/{configId}/pullImage", produces = [MediaType.APPLICATION_NDJSON_VALUE])
+    suspend fun pullImage(
+        @RequestBody request: ImageRequest,
+        @PathVariable("configId") configId: ConfigId,
+    ): Flow<PullResponseItem> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.pullImage(request)
+        return result
+    }
+
+    @PostMapping("/image/{configId}/pushImage", produces = [MediaType.APPLICATION_NDJSON_VALUE])
+    suspend fun pushImage(
+        @RequestBody request: ImageRequest,
+        @PathVariable("configId") configId: ConfigId
+    ): Flow<PushResponseItem> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.pushImage(request)
+        return result
+    }
+
+    @PostMapping("/image/{configId}/createImage")
+    fun createImage(
+        @RequestParam("repo") repo: String,
+        @RequestParam("file") file: MultipartFile,
+        @PathVariable("configId") configId: ConfigId
+    ): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.createImage(repo, file.inputStream)
+        return if (result != null)
+            ResponseEntity(result, HttpStatus.OK)
+        else
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @PostMapping("/image/{configId}/loadImage")
+    suspend fun loadImage(
+        @RequestParam("file") file: MultipartFile,
+        @PathVariable("configId") configId: ConfigId
+    ): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.loadImage(file.inputStream)
+        return if (result != null)
+            ResponseEntity(result, HttpStatus.OK)
+        else
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @GetMapping("/image/{configId}/searchImages")
+    fun searchImages(
+        @PathVariable("configId") configId: ConfigId,
+        @RequestParam("search") term: String
+    ): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.searchImages(term)
+        return if (result != null)
+            ResponseEntity(result, HttpStatus.OK)
+        else
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @DeleteMapping("/image/{configId}/removeImage")
+    fun removeImage(
+        @PathVariable("configId") configId: ConfigId,
+        @RequestParam("id") id: String
+    ): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.removeImage(id)
+        return if (result)
+            ResponseEntity(HttpStatus.OK)
+        else
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @GetMapping("/image/{configId}/listOfImages")
+    fun getListOfImages(@PathVariable("configId") configId: ConfigId): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.listOfImages()
+        return if (result != null)
+            ResponseEntity(result, HttpStatus.OK)
+        else
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @GetMapping("/image/{configId}/inspectImage")
+    fun inspectImage(
+        @PathVariable("configId") configId: ConfigId,
+        @RequestParam("id") id: String
+    ): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.inspectImage(id)
+        return if (result != null)
+            ResponseEntity(result, HttpStatus.OK)
+        else
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @PostMapping("/image/{configId}/saveImage")
+    fun saveImage(
+        @PathVariable("configId") configId: ConfigId,
+        @RequestBody request: ImageRequest,
+    ): ResponseEntity<Any> {
+        val apiService = apiServices[configId]
+            ?: throw RuntimeException("<b525a66c> Api service for $configId is not found")
+        val result = apiService.saveImage(request)
+        return if (result != null)
+            ResponseEntity(result, HttpStatus.OK) //TODO return as file
+        else
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+}
